0fba97f029f5f854fd7f6661e83f40938cbcff2c
LENOVO <LENOVO@DESKTOP-CG70URT>
2019-03-02 20:10:55 +0000

    init project3

--- src/main/java/com/poussin/production/ProductionApplication.java
+++ src/main/java/com/poussin/production/ProductionApplication.java
@@ -2,8 +2,10 @@ package com.poussin.production;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.poussin.production.rest")
 public class ProductionApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/poussin/production/rest/proxy/FirmeProxy.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.poussin.production.rest.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ *
+ * @author LENOVO
+ */
+//@FeignClient(name="", url="")
+public interface FirmeProxy {
+    
+}
